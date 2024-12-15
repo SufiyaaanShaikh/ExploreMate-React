916558fd1d5768e2a4e946c45dc4d170d9e207a3
sufiyan <sufisk2526@gmail.com>
2024-12-15 23:02:47 +0530

    initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './css/App.css';
+import './css/utility.css'
+import Login from './Login';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <a href='Login.jsx'> Login</a> */}
+      <Login/>
     </div>
   );
 }
