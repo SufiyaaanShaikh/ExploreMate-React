328aac375076454bf1a5239258a9f4ea7b760651
SufiyaaanShaikh <sufisk2526@gmail.com>
2025-01-17 22:15:07 +0530

    Scroll To Top added but not working

--- src/components/Review/WriteReview.jsx
+++ src/components/Review/WriteReview.jsx
@@ -5,19 +5,6 @@ function WriteReview() {
   return (
     <>
       {/* <!-- Main start  --> */}
-      {/* <section id="review" class="position">
-        <div class="sec"> */}
-          {/* <div class="title flex items-center">
-                <div class=" grid">
-                    <h2 class="fw-700 sec-head">Write Your Review</h2>
-                    <p class="para-f para-c para-f">Share your experience with Like-Minded Explorers.</p>
-                </div>
-                <a href="#" class="write flex justify-content items-center">
-                    <img src="images/pen.svg" alt="pen">
-                    <p class="para-c para-f">Write a review</p>
-                </a>
-            </div> */}
-
           <div class="write-review grid">
             <div class="left">
               <div class="review-photo">
@@ -78,8 +65,6 @@ function WriteReview() {
               </form>
             </div>
           </div>
-        {/* </div>
-      </section> */}
       {/* <!-- Main end  --> */}
     </>
   );
